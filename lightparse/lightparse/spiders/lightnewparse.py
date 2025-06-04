ec57b5d7794ddd2642c8d5fbe10cd03c837fec7f
Николай <kirpix524@gmail.com>
2025-06-04 15:04:54 +0400

    simple parser of lamps on divan.ru

--- lightparse/lightparse/spiders/lightnewparse.py
+++ lightparse/lightparse/spiders/lightnewparse.py
@@ -7,10 +7,27 @@ class LightnewparseSpider(scrapy.Spider):
     start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
-        lamps = response.css("div._Ud0k")
-        for lamp in lamps:  #
+        # 1. Проходим по всем карточкам товара (у каждой есть класс "_Ud0k")
+        for lamp in response.css("div._Ud0k"):
+            # 2. Название лежит в <span itemprop="name">…</span>
+            name = lamp.css("span[itemprop='name']::text").get()
+
+            # 3. Цена. Чаще всего берут первое вхождение data-testid="price".
+            #    Это вернёт строку вида "4990" (по умолчанию без "руб.")
+            price = lamp.css("span.KIkOH[data-testid='price']::text").get()
+
+            # 4. Ссылка на товар. У тега <a> с классом ProductName есть href="/product/..."
+            relative_url = lamp.css("a.ProductName::attr(href)").get()
+            #    Если ссылок несколько, .get() выдаст первую.
+            #    Например: "/product/podvesnoj-svetilnik-ferum-orange"
+            #    Собираем абсолютный URL:
+            detail_url = response.urljoin(relative_url) if relative_url else None
+
+            # 5. Теперь «yield»им результат
             yield {
-                "name": lamp.css("h2._Ud0k_ _title::text").get(),
-                "price": lamp.css("span._Ud0k__price::text").get(),
-                "link": lamp.css("a._Ud0k__link::attr(href)").get(),
+                "name": name,
+                "price": price,
+                "detail_url": detail_url
             }
+
+#<span class="ui-LD-ZU KIkOH" data-testid="price">4990<span class="ui-i5wwi ui-VDyJR ui-VWOa-">руб.</span></span>
